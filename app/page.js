090a223ed2a484cea994d75d779111aac8df6b0c
Veeraldo <Veraldo24092005@gmail.com>
2025-12-31 12:11:21 +0700

    Update page.js

--- app/page.js
+++ app/page.js
@@ -550,7 +550,7 @@ export default function Home() {
                     style={{
                         position: 'fixed',
                         bottom: '40px',
-                        left: '40px',
+                        right: '40px',
                         width: '50px',
                         height: '50px',
                         borderRadius: '50%',
